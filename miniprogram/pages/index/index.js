d2b3e98d5e7a7576c9d5c9532fc513c0aa1c0f98
liaoqinwei <2573424017@qq.com>
2020-10-14 17:03:12 +0800

    10/14

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,79 +1,106 @@
-// miniprogram/pages/index.js
-Page({
+import * as echarts from '../../ec-canvas/echarts';
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+let chart = null;
 
-  },
+function initChart(canvas, width, height, dpr) {
+  chart = echarts.init(canvas, null, {
+    width: width,
+    height: height,
+    devicePixelRatio: dpr // new
+  });
+  canvas.setChart(chart);
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
-
-
-  /**
-   * 授权客户信息
-   */
-  authorizeHandle(){
-    wx.getUserInfo({
-      lang: 'zh_CN',
-      success(res){
-        console.log(res)
+  var option = {
+    title: {
+      text: '折线图堆叠'
+    },
+    tooltip: {
+      trigger: 'axis'
+    },
+    legend: {
+      data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
+    },
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true
+    },
+    toolbox: {
+      feature: {
+        saveAsImage: {}
       }
-    })
+    },
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [{
+        name: '邮件营销',
+        type: 'line',
+        stack: '总量',
+        data: [120, 132, 101, 134, 90, 230, 210],
+        smooth:true
+      },
+      {
+        name: '联盟广告',
+        type: 'line',
+        stack: '总量',
+        data: [220, 182, 191, 234, 290, 330, 310],
+        smooth:true
+      },
+      {
+        name: '视频广告',
+        type: 'line',
+        stack: '总量',
+        data: [150, 232, 201, 154, 190, 330, 410],
+        smooth:true
+      },
+      {
+        name: '直接访问',
+        type: 'line',
+        stack: '总量',
+        data: [320, 332, 301, 334, 390, 330, 320],
+        smooth:true
+      },
+      {
+        name: '搜索引擎',
+        type: 'line',
+        stack: '总量',
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        smooth:true,
+        symbolSize:10
+      }
+    ]
   }
-})
+
+  chart.setOption(option);
+  return chart;
+}
+
+Page({
+  onShareAppMessage: function (res) {
+    return {
+      title: 'ECharts 可以在微信小程序中使用啦！',
+      path: '/pages/index/index',
+      success: function () {},
+      fail: function () {}
+    }
+  },
+  data: {
+    ec: {
+      onInit: initChart
+    }
+  },
+
+  onReady() {
+    setTimeout(function () {
+      // 获取 chart 实例的方式
+      // console.log(chart)
+    }, 2000);
+  }
+});
